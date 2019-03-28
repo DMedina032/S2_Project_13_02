a2a4f4b78390f6f88d3eda21c1c51fe32ea2f24d
Unknown <dmedina032@west-mec.org>
2019-03-27 18:17:10 -0700

    final  commit/ submit for review
    I created switch cases for the values, delete, arrowup, and enter. I also got elements by their id's  and finished my comments.

--- mt_calc.js
+++ mt_calc.js
@@ -33,21 +33,57 @@
 
 */
 window.onload = init;
-
+// Initializes the contents of the web page and sets up the event handlers
+// Within the function init() I declared the varible calcButtons and got the element by the class name from the html document. I then created a for loop the varible calcButtons and within this, I then put[i] for the array and gave it an onclick even and set the value to buttonClick. From the calcKeys I was then able to go into the array and add an event listener for keydown and for the value I was thn able to get the element by the id  foe calcWindow and added an event listener for keydown and calckeys.
 function init() {
       var calcButtons = document.getElementsByClassName("calcButton");
-      for (var i = 0; i < calcButtons; i++) {
+      for (var i = 0; i < calcButtons.length; i++) {
             calcButtons[i].onclick = buttonClick;
       }
       calcKeys[i].addEventListener("keydown") = document.getElementById("calcWindow").addEventListener("keydown", calcKeys);
 }
-
+// buttonClick(e) Adds functions to the buttons clicked within the calcutlor. I declared the varibles calcValue ,calcDecimal, and buttonValue and got the elemnets by the id's and created a sitch case for the calcValue.  eraseChar(textStr) Erases the last character from the text string, textStr. evalEq(textStr, decimals) Evaluates the equation in textStr, returning a value to the number of decimals specified by the decimals parameter.  lastEq(textStr) Returns the previous expression from the list of expressions in the textStr parameter.
 function buttonClick(e) {
-      var calcValue = document.getElementById("");
-
+      var calcValue = document.getElementById("calcWindow").value;
+      var calcDecimal = document.getElementById("decimals").value;
+      var buttonValue = e.target.value;
+      switch (buttonValue) {
+            case "del":
+                  calcValue = "";
+                  break;
+            case "bksp":
+                  calcValue = eraseChar(calcValue)
+                  break;
+            case "enter":
+                  calcValue += "=" + evalEq(calcValue, calcDecimal) + "\n";
+                  break;
+            case "perv":
+                  calcValue += lastEq(calcValue) + "\n";
+                  break;
+            default:
+                  calcValue += buttonValue;
+                  break;
+      }
+      document.getElementById("calcWindow").value = calcValue;
+      document.getElementById("calcWindow").focus();
 }
-
-
+//  calcKeys(e) Adds functions to key pressed within the calculator window 
+function calcKeys(e) {
+      var calcValue = document.getElementById("calcWindow").value;
+      var calcDecimal = document.getElementById("decimals").value;
+      switch (e.key) {
+            case "Delete":
+                  calcValue = "";
+                  break;
+            case "enter":
+                  calcValue += "=" + evalEq(calcValue, calcDecimal);
+                  break;
+            case "ArrowUp":
+                  calcValue += evalEq(calcWindow.value);
+                  e.preventDefault();
+      }
+      document.getElementById("calcWindow").value = calcValue;
+}
 
 
 /* ===================================================================== */
